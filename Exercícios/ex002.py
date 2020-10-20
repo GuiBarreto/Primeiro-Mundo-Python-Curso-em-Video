44aa7a16d8f43a38f7e06d7d38467c5ccd99cec9
Guilherme Barreto <barretogui90@gmail.com>
2020-10-20 17:30:09 -0300

    Correção do comentário

--- Exercícios/ex002.py
+++ Exercícios/ex002.py
@@ -1,4 +1,4 @@
-#COMPRIMENTAR O USUÁRIO
+#CUMPRIMENTAR O USUÁRIO
 nome = input('digite seu nome:')
 idade = input('você tem quantos anos? ')
 print ('Prazer em te conhecer, {} de {} anos!'.format(nome, idade) )

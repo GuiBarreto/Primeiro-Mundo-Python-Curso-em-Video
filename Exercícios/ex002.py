c1b5a59f233762ef6b147dcf2d012c11138a1fe6
Guilherme Barreto <barretogui90@gmail.com>
2020-10-19 21:11:57 -0300

    criando exercício 002

---
+++ Exercícios/ex002.py
@@ -0,0 +1,4 @@
+#perguntando nome e idade e comprimentando
+nome = input('digite seu nome:')
+idade = input('você tem quantos anos? ')
+print ('Prazer em te conhecer, {} de {} anos!'.format(nome, idade) )
